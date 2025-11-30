adfaa01cb30b0e97b8009b149d8a66322172fef2
Lorelvis Santos <santoslorelvis@gmail.com>
2025-11-30 16:36:08 -0400

    feat: initialize React app with TypeScript, Tailwind CSS, and Vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { menuItems } from "./data/db"
+
+function App() {
+  return (
+    <>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-bold">Calculadora de Propinas y Consumo</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
